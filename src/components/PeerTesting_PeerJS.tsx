24a61d0be7480dccf17b05437c143d3700ea2edf
Tiberiu <musattiberiu@gmail.com>
2024-02-16 15:37:32 +0100

    metered ice servers

--- src/components/PeerTesting_PeerJS.tsx
+++ src/components/PeerTesting_PeerJS.tsx
@@ -9,31 +9,7 @@ export default function PeerTesting({ iceServer }: { iceServer: IceServer }) {
   const peerRef = useRef<Peer>();
   const peerOptions = {
     config: {
-      iceServers: [
-        {
-          urls: "stun:stun.relay.metered.ca:80",
-        },
-        {
-          urls: "turn:standard.relay.metered.ca:80",
-          username: "7316a86e689702ed0f208cb0",
-          credential: "nvZJtXeLiLiv37W1",
-        },
-        {
-          urls: "turn:standard.relay.metered.ca:80?transport=tcp",
-          username: "7316a86e689702ed0f208cb0",
-          credential: "nvZJtXeLiLiv37W1",
-        },
-        {
-          urls: "turn:standard.relay.metered.ca:443",
-          username: "7316a86e689702ed0f208cb0",
-          credential: "nvZJtXeLiLiv37W1",
-        },
-        {
-          urls: "turns:standard.relay.metered.ca:443?transport=tcp",
-          username: "7316a86e689702ed0f208cb0",
-          credential: "nvZJtXeLiLiv37W1",
-        },
-      ],
+      iceServers: [iceServer],
     },
     debug: 3,
   };
